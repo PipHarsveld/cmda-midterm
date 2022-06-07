6164dcf041711242c2684ae37b7c7ee12441bcee
KaivWezel <66356377+KaivWezel@users.noreply.github.com>
2022-06-07 12:17:26 +0200

    fixed slice error

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,7 @@
 </template>
 
 <script>
-import CardCon from '~/components/CardCon.vue'
 export default {
-  components: {
-    CardCon,
-  },
-
   name: 'IndexPage',
 
   async asyncData({ $prismic, params, error }) {
