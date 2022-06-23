bed15c1ab8d2e09965c34012b7709e063a75a195
Bommezijn <nathanbommezijn@gmail.com>
2022-06-23 10:57:23 +0200

    wip: Progression on dynamic index
    BREAKING CHANGE: NOT FINISHED

---
+++ pages/OldIndex.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>test</p>
+    <!-- <div v-for="item in"></div> -->
+    <div v-html="$prismic.asHTML(document.data.title)"></div>
+    {{ document.data }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OldIndex',
+  data() {
+    return {
+      document: {},
+    }
+  },
+  async fetch() {
+    this.document = await this.$prismic.api.getSingle('homepage')
+  },
+}
+</script>
+<style>
+div {
+  margin-top: 6em;
+}
+</style>
